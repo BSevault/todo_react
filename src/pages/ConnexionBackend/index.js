63fdb93eec5634689d45238f47f341151bbe1bae
BSevault <b.sevault@gmail.com>
2022-01-27 14:12:35 +0100

    ajout d'un élement pour ajouter une todo

--- src/pages/ConnexionBackend/index.js
+++ src/pages/ConnexionBackend/index.js
@@ -1,9 +1,11 @@
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 
 const ConnexionBackend = () => {
 
     const [todos, setTodos] = useState();
+
+    const textRef = useRef();
 
     const getTodos = async () => {
         const result = await axios.get('http://localhost:8000/api/todos/');
@@ -11,17 +13,34 @@ const ConnexionBackend = () => {
         console.log(result.data.success);
     }
 
+    const postTodo = async (event) => {
+        event.preventDefault();     //empeche la page de se recharger lors du submit du form
+        const result = await axios.post('http://localhost:8000/api/todos', { texte: textRef.current.value });
+        const { insertId } = result.data.success;
+        setTodos(prevstate => { 
+            return [ ...prevstate, { id: insertId,  texte: textRef.current.value } ]
+        });
+        textRef.current.value = '';
+        textRef.current.focus();
+        console.log(todos);
+    }
+
     useEffect( () => {
-        getTodos();
+        getTodos()
+        console.log(textRef.current);
     }, [] );
 
     return (
          <div>
+         <form>
+             <input type="text" ref={textRef}/>
+             <input type="submit" value="Créer" onClick={ (event) => postTodo(event) } />
+         </form>
             <ul>
                 { // {} pour interpréter du js dans du html
 
                     todos && todos.map( (todo) => {
-                        return <li key={`${todo.id}`} >{todo.texte}</li>
+                        return <li key={todo.id}>{todo.texte}</li>
                     })
                 }
             </ul>
