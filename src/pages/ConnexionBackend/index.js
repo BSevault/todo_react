04777aae2416ea0fb632f1d458d695ceb0063cfd
BSevault <b.sevault@gmail.com>
2022-01-27 09:37:54 +0100

    created controllers

---
+++ src/pages/ConnexionBackend/index.js
@@ -0,0 +1,32 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react';
+
+const ConnexionBackend = () => {
+
+    const [todos, setTodos] = useState();
+
+    const getTodos = async () => {
+        const result = await axios.get('http://localhost:8000/api/todos/');
+        setTodos(result.data.success);
+        console.log(result.data.success);
+    }
+
+    useEffect( () => {
+        getTodos();
+    }, [] );
+
+    return (
+         <div>
+            <ul>
+                { // {} pour interpréter du js dans du html
+
+                    todos && todos.map( (todo) => {
+                        return <li key={`${todo.id}`} >{todo.texte}</li>
+                    })
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default ConnexionBackend;
