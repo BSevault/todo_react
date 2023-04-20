01a82bdaa2bb6102c6359fc816d28475683bee35
BSevault <b.sevault@gmail.com>
2023-04-20 21:52:36 +0200

    ?

--- src/components/Effect/index.js
+++ src/components/Effect/index.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import useAlert from '../../hooks/useAlert';
 
 function Effect () {
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
+  useEffect( () => setCount2(count2+1), [count])
 
-  useAlert("Mon composant Effect est détruit", count);
+  useAlert("Mon composant Effect est détruit", count2);
 
   return (
     <div>
@@ -12,6 +14,7 @@ function Effect () {
       <button onClick={() => setCount(count + 1)}>
         Cliquez ici
       </button>
+      <p>Test : {count2} </p>
     </div>
   );
 }
--- src/hooks/useAlert.js
+++ src/hooks/useAlert.js
@@ -5,7 +5,7 @@ const useAlert = (message, counter) => {
     useEffect(() => {
         
         if (counter > 3) {
-            alert("Attention le composant à été détruis plus de 3 fois")
+            alert("Attention le composant à été détruit plus de 3 fois")
         }
 
         return () => alert(message);
